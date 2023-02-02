499e2787035793c1e9955419e911f7de8c92ad5f
loony <wooounding@gmail.com>
2023-02-02 20:16:36 +0300

    правки линтера

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -8,7 +8,7 @@ router.register('posts', PostViewSet)
 router.register('groups', GroupViewSet)
 router.register('follow', FollowViewSet, basename='follow')
 router.register(
-    'posts/(?P<post_id>\d+)/comments',
+    r'posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
     basename='comments')
 
